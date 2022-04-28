56b5c8f014fe7cb45d716a9439b7b6fd9836fcb5
Sharan-km-sb <104551925+Sharan-km-sb@users.noreply.github.com>
2022-04-28 22:27:59 +0530

    Add files via upload

---
+++ Calculator app/app.js
@@ -0,0 +1,71 @@
+const histroy = document.querySelector('.histroy ');
+const output = document.getElementById('out');
+const result = document.getElementById('result');
+const keys = document.querySelectorAll('button');
+const display = document.querySelector('.arrow');
+const hdisplay = document.getElementById('hdisplay');
+const delete_btn = document.getElementById('delete');
+const ul = document.getElementById('histroy-value-list');
+const clear = document.querySelector('#all-clear');
+
+clear.addEventListener('click',() =>{
+    result.innerText = "0";
+    histroy.innerText = null;
+    output.textContent = "0"
+});
+
+
+
+delete_btn.addEventListener('click', () => {
+     
+         ul.innerText = null;
+
+});
+
+display.addEventListener('click', () => {
+
+    hdisplay.classList.toggle('active');
+}
+);
+
+for (const button of keys) {
+    button.addEventListener('click', calculation);
+}
+
+function calculation() {
+    let buttonText = this.innerText;
+    console.log(buttonText);
+     
+        
+        
+ 
+    if (buttonText === "=") {
+        result.innerText = eval(output.innerText);
+        histroy.innerText = result.innerText;
+        let results = result.innerText;
+
+        addhistroy();
+
+        function addhistroy() {
+            
+            let li = document.createElement("li");
+            li.setAttribute('id', results);
+            li.appendChild(document.createTextNode(results));
+            ul.appendChild(li);
+
+        }
+
+
+    }
+    else {
+
+        output.textContent += buttonText;
+        return;
+    }
+
+}
+
+
+
+
+
