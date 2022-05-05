8c4e33643c86344edfb8fe2e68c760edf9f22db0
Sharan-km-sb <104551925+Sharan-km-sb@users.noreply.github.com>
2022-05-05 14:35:05 +0530

    Update app.js

--- Calculator app/app.js
+++ Calculator app/app.js
@@ -4,9 +4,16 @@ const result = document.getElementById('result');
 const keys = document.querySelectorAll('button');
 const display = document.querySelector('.arrow');
 const hdisplay = document.getElementById('hdisplay');
-const delete_btn = document.getElementById('delete');
 const ul = document.getElementById('histroy-value-list');
 const clear = document.querySelector('#all-clear');
+const deleteAll = document.getElementById("deleteAll");
+const table = document.getElementById("myTableData");
+
+deleteAll.addEventListener('click' ,(e) => {
+             
+location.reload();
+
+});
 
 clear.addEventListener('click',() =>{
     result.innerText = "0";
@@ -16,17 +23,7 @@ clear.addEventListener('click',() =>{
 
 
 
-delete_btn.addEventListener('click', () => {
-     
-         ul.innerText = null;
 
-});
-
-display.addEventListener('click', () => {
-
-    hdisplay.classList.toggle('active');
-}
-);
 
 for (const button of keys) {
     button.addEventListener('click', calculation);
@@ -34,35 +31,42 @@ for (const button of keys) {
 
 function calculation() {
     let buttonText = this.innerText;
+    
+
     console.log(buttonText);
-     
-           
-     
+  
     if(buttonText === "DEL" ){
         output.textContent = output.textContent.substring(0, (output.textContent.length-1));
         return;
-
+             
     }
+
         
  
     if (buttonText === "=") {
+        
         result.innerText = eval(output.innerText);
         let results = result.innerText;
         histroy.innerText = output.innerText+''+ '='+''+ results;
-     
+        
+
         addhistroy();
 
         function addhistroy() {
             
-            let li = document.createElement("li");
-            li.setAttribute('id', results);
-            li.setAttribute('style' , "float:left");
-            li.appendChild(document.createTextNode(output.innerText+''+ '='+''+ results));
-            ul.appendChild(li);
+          
+       
+          var rowCount = table.rows.length;
+          var row = table.insertRow(rowCount);
+       
+          
+          row.insertCell(0).innerHTML= output.textContent;
+          row.insertCell(1).innerHTML= result.textContent;
+          row.insertCell(2).innerHTML= '<input type="button" style="color:white; cursor:pointer; padding:3px 3px; background-color: #362a89; border:1 px solid  #362a89;" value = "Del" onClick="Javacsript:deleteRow(this)">';
 
         }
-
-
+        
+     
     }
     else {
 
@@ -70,6 +74,14 @@ function calculation() {
         return;
     }
 
+
+}
+function deleteRow(obj) {
+            
+    var index = obj.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("myTableData");
+    table.deleteRow(index);
+    
 }
 
 
